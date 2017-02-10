d3af237a2b376ae2c683c571be86b94c90022b28
Sebastian Thiebaud <sebcthiebaud@gmail.com>
2017-02-10 11:07:57 -0800

    Update Distance.playground
    Use of Struct instead of Enum

--- Distance.playground/Contents.swift
+++ Distance.playground/Contents.swift
@@ -1,173 +1,130 @@
 public
-enum Distance
+struct Distance
 {
-    case feet(Double)
-    case kilometers(Double)
-    case meters(Double)
-    case miles(Double)
-    
-    
-    private
-    enum ConversionConstant: Double
-    {
-        case feetToKilometers   = 0.0003048
-        case feetToMeters       = 0.3048
-        case feetToMiles        = 0.000189394
-        case kilometersToFeet   = 3280.84
-        case kilometersToMeters = 1000.0
-        case kilometersToMiles  = 0.6213713910761
-        case metersToFeet       = 3.28084
-        case metersToKilometers = 0.001
-        case metersToMiles      = 0.000621371192
-        case milesToFeet        = 5280.0
-        case milesToKilometers  = 1.60934
-        case milesToMeters      = 1609.34
-    }
-    
-    
+    // Base unit is meters
+    fileprivate var metersValue: Double = 0
+
     public
-    func toFeet() -> Double
+    init(_ length: Double, unit: Unit)
     {
-        switch self
+        self.metersValue = length * unit.coefficient
+    }
+
+    public
+    enum Unit: String
+    {
+        case feet       = "ft"
+        case kilometers = "km"
+        case meters     = "m"
+        case miles      = "mi"
+
+        internal var coefficient: Double
         {
-        case .feet(let d):
-            return d
-        case .kilometers(let d):
-            return d * ConversionConstant.kilometersToFeet.rawValue
-        case .meters(let d):
-            return d * ConversionConstant.metersToFeet.rawValue
-        case .miles(let d):
-            return d * ConversionConstant.milesToFeet.rawValue
+            switch self
+            {
+            case .feet:
+                return 0.3048
+            case .kilometers:
+                return 1000
+            case .meters:
+                return 1
+            case .miles:
+                return 1609.34
+            }
         }
     }
-    
+
     public
-    func toKilometers() -> Double
+    func `in`(_ unit: Unit) -> Double
     {
-        switch self
-        {
-        case .feet(let d):
-            return d * ConversionConstant.feetToKilometers.rawValue
-        case .kilometers(let d):
-            return d
-        case .meters(let d):
-            return d * ConversionConstant.metersToKilometers.rawValue
-        case .miles(let d):
-            return d * ConversionConstant.milesToKilometers.rawValue
-        }
+        return self.metersValue / unit.coefficient
     }
-    
+
     public
-    func toMeters() -> Double
+    func inUnits(unit: Unit) -> Double
     {
-        switch self
-        {
-        case .feet(let d):
-            return d * ConversionConstant.feetToMeters.rawValue
-        case .kilometers(let d):
-            return d * ConversionConstant.kilometersToMeters.rawValue
-        case .meters(let d):
-            return d
-        case .miles(let d):
-            return d * ConversionConstant.milesToMeters.rawValue
-        }
-    }
-    
-    public
-    func toMiles() -> Double
-    {
-        switch self
-        {
-        case .feet(let d):
-            return d * ConversionConstant.feetToMiles.rawValue
-        case .kilometers(let d):
-            return d * ConversionConstant.kilometersToMiles.rawValue
-        case .meters(let d):
-            return d * ConversionConstant.metersToMiles.rawValue
-        case .miles(let d):
-            return d
-        }
+        return `in`(unit)
     }
 }
 
 extension Distance: Comparable { }
 
 public
-func ==(lhs: Distance, rhs: Distance) -> Bool
+func == (lhs: Distance, rhs: Distance) -> Bool
 {
-    return lhs.toMeters() == rhs.toMeters()
+    return lhs.in(.meters) == rhs.in(.meters)
 }
 
 public
-func <(lhs: Distance, rhs: Distance) -> Bool
+func < (lhs: Distance, rhs: Distance) -> Bool
 {
-    return lhs.toMeters() < rhs.toMeters()
+    return lhs.in(.meters) < rhs.in(.meters)
 }
 
 public
-func <=(lhs: Distance, rhs: Distance) -> Bool
+func <= (lhs: Distance, rhs: Distance) -> Bool
 {
-    return lhs.toMeters() <= rhs.toMeters()
+    return lhs.in(.meters) <= rhs.in(.meters)
 }
 
 public
-func >(lhs: Distance, rhs: Distance) -> Bool
+func > (lhs: Distance, rhs: Distance) -> Bool
 {
-    return lhs.toMeters() > rhs.toMeters()
+    return lhs.in(.meters) > rhs.in(.meters)
 }
 
 public
-func >=(lhs: Distance, rhs: Distance) -> Bool
+func >= (lhs: Distance, rhs: Distance) -> Bool
 {
-    return lhs.toMeters() >= rhs.toMeters()
+    return lhs.in(.meters) >= rhs.in(.meters)
 }
 
 public
-func +(lhs: Distance, rhs: Distance) -> Distance
+func + (lhs: Distance, rhs: Distance) -> Distance
 {
-    return Distance.meters(lhs.toMeters() + rhs.toMeters())
+    return Distance(lhs.`in`(.meters) + rhs.in(.meters), unit: .meters)
 }
 
 public
-func -(lhs: Distance, rhs: Distance) -> Distance
+func - (lhs: Distance, rhs: Distance) -> Distance
 {
-    return Distance.meters(lhs.toMeters() - rhs.toMeters())
+    return Distance(lhs.in(.meters) - rhs.in(.meters), unit: .meters)
 }
 
 public
-func /(lhs: Distance, rhs: Double) -> Distance
+func / (lhs: Distance, rhs: Double) -> Distance
 {
-    return Distance.meters(lhs.toMeters() / rhs)
+    return Distance(lhs.in(.meters) / rhs, unit: .meters)
 }
 
 public
-func *(lhs: Distance, rhs: Double) -> Distance
+func * (lhs: Distance, rhs: Double) -> Distance
 {
-    return Distance.meters(lhs.toMeters() * rhs)
+    return Distance(lhs.in(.meters) * rhs, unit: .meters)
 }
 
 public
-func *(lhs: Double, rhs: Distance) -> Distance
+func * (lhs: Double, rhs: Distance) -> Distance
 {
-    return Distance.meters(lhs * rhs.toMeters())
+    return Distance(lhs * rhs.in(.meters), unit: .meters)
 }
 
 // MARK: Tests
 
-let one_meter       = Distance.meters(1)
-let thousand_meters = Distance.meters(1000)
-let twelve_miles    = Distance.miles(12)
-let one_kilometer   = Distance.kilometers(1)
-let five_feet       = Distance.feet(5)
+let one_meter       = Distance(1, unit: .meters)
+let thousand_meters = Distance(1000, unit: .meters)
+let twelve_miles    = Distance(12, unit: .miles)
+let one_kilometer   = Distance(1, unit: .kilometers)
+let five_feet       = Distance(5, unit: .feet)
 
-five_feet.toMeters()
-print("Twelve miles equal to \(twelve_miles.toKilometers()) kilometers")
-print("Twelve miles equal to \(twelve_miles.toFeet()) feet")
+five_feet.in(.meters)
+print("Twelve miles equal to \(twelve_miles.in(.kilometers)) kilometers")
+print("Twelve miles equal to \(twelve_miles.in(.feet)) feet")
 
 print("Is a thousand meters equal to one kilometer? \(thousand_meters == one_kilometer)")
 print("Are twelve miles greater than five feet? \(twelve_miles > five_feet)")
 print("Are five feet less than or equal to a meter? \(five_feet <= one_meter)")
 
-print("What's twelve miles plus a thousand meters? \((twelve_miles + thousand_meters).toMiles()) miles")
-print("What's five feet times five? \((five_feet * 5).toFeet()) feet")
-print("What's one kilometer divided by two? \((one_kilometer / 2).toMeters()) meters")
+print("What's twelve miles plus a thousand meters? \((twelve_miles + thousand_meters).in(.miles)) miles")
+print("What's five feet times five? \((five_feet * 5).in(.feet)) feet")
+print("What's one kilometer divided by two? \((one_kilometer / 2).in(.meters)) meters")
